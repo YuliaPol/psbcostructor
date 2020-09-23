4535c5d8ff9a2c94bf86602c94ae6a32e83b12d4
YuliaPol <julia.polyeshko@gmail.com>
2020-09-23 14:37:05 +0300

    Add Russin for datepicker

--- js/constructor.js
+++ js/constructor.js
@@ -1,5 +1,25 @@
 jQuery(function ($) {
     $(document).ready(function () {
+        $.datepicker.setDefaults(
+            {
+            closeText: 'Закрыть',
+            prevText: '',
+            currentText: 'Сегодня',
+            monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+                'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+            monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+                'Июл','Авг','Сен','Окт','Ноя','Дек'],
+            dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+            dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+            dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+            weekHeader: 'Не',
+            dateFormat: 'dd.mm.yy',
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: ''
+        });
+
         $( ".listbox li" ).draggable({
             helper: "clone",
             cursor: "move",
