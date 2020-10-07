fccfbe4949bf4bcdd6f9cd34e8d4ccb6e76a3dd7
YuliaPol <julia.polyeshko@gmail.com>
2020-10-07 12:08:18 +0300

    hellopage, dragable

---
+++ js/hellopage.js
@@ -0,0 +1,59 @@
+jQuery(function ($) {
+    $(document).ready(function () {
+        //Make element draggable
+        $(".drag").draggable({
+            appendTo: ".dragable",
+            containment: ".dragable",
+            grid: [ 10, 10 ],
+            stop: function( event, ui ) {
+                console.log(event);
+                console.log(ui);
+            }
+        });
+        $( ".dragimage" ).resizable({
+            containment: ".dragable",
+            grid: [ 10, 10 ],
+            aspectRatio: true,
+            handles: "n, e, s, w"
+        });
+        $( ".dragtext" ).resizable({
+            autoHide: false,
+            containment: ".dragable",
+            grid: [ 10, 10 ],
+            handles: "n, e, s, w"
+        });
+        // $(".droppable").droppable({
+        //     drop: function (e, ui) {
+        //         if ($(ui.draggable)[0].id != "") {
+        //             x = ui.helper.clone();
+        //             ui.helper.remove();
+        //         x.draggable({
+        //             //helper: 'original',
+        //             containment: '.droppable',
+        //             tolerance: 'fit',
+        //             stack: '.drag'
+        //         });
+
+        //         x.resizable({
+        //           animate: true,
+        //           //aspectRatio: 16 / 9,
+        //           helper: "ui-resizable-helper",
+        //           handles: "n, e, s, w, nw, ne, sw,se"
+        //         });
+        //         x.appendTo('.droppable');
+        //         }
+        //     }
+        // });
+    });
+});
+$(document).click(function(e) {
+    // matches all children of droppable, change selector as needed
+    if( $(e.target).is(".droppable .drag") ) {
+        $(e.target).find(".ui-resizable-handle").show();
+		$(".tools").show();
+    }
+    else {
+        $(".droppable").find(".ui-resizable-handle").hide();
+		$(".tools").hide();
+    }
+});
