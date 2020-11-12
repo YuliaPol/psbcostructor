5c6cb56f2caed5d8421ca545a7e0eaa1bd1e426c
YuliaPol <julia.polyeshko@gmail.com>
2020-11-12 17:10:01 +0200

    add simple bar chart, correct radial bar

--- js/charts.js
+++ js/charts.js
@@ -1,6 +1,56 @@
 
-jQuery(function ($) {
-    $(document).ready(function () {
-       
-    });
-});
+if(chartData.length > 0){
+    for(let i = 0; i < chartData.length; i++){
+        if(chartData[i].element && chartData[i].data){
+            var data = chartData[i].data;
+            if(chartData[i].type == 'radialbar'){
+                $(chartData[i].element).radialBar({
+                    data: data,
+                    width: "250",
+                    height: "250",
+                    padding: 10,
+                });
+            }
+            if(chartData[i].type == 'simpleBar'){
+                drawSimpleBar($(chartData[i].element), data);
+            }
+        }
+    }
+}
+function drawSimpleBar(element, data) {
+    if(data.length > 0){
+        var percent = new Array(data.length);
+        var summary = 0;
+        var maxValue = 0;
+        for (let i = 0; i < data.length; i++) {
+            summary = summary + parseInt(data[i].progress);
+            if(parseInt(data[i].progress) > maxValue){
+                maxValue = parseInt(data[i].progress);
+            }
+        }
+
+        var percentFill = new Array(data.length);
+        var relativeFill = 100 / maxValue;
+            
+        var relative = 100 / summary;
+        for (let i = 0; i < data.length; i++) {
+            percent[i] = Math.round(relative*data[i].progress);
+            percentFill[i] = Math.round(relativeFill*data[i].progress*0.9);
+        }
+        var elements = '<div class="barlist">';
+        for (let i = 0; i < data.length; i++) {
+            var top = 100 - percentFill[i];
+            elements = elements + 
+            '<div class="baritem">'
+            +'    <div class="barcol">'
+            +'      <div class="barcolFill" style="background-color: '+ data[i].background + '; top: ' + top + '%"></div>'
+            +'      <div class="value">' + percent[i] + '% / '+ data[i].progress + ' шт</div>'
+            +'  </div>'
+            +'  <div class="label">оценкa <span class="bold">' + data[i].labelText + '</span></div>'
+            +'</div>';
+        }
+        var elements = elements + '</div>';
+        element.addClass('.simplebar');
+        $(elements).appendTo(element);
+    }
+}
--- js/jqueryRadialBarMultiple.js
+++ js/jqueryRadialBarMultiple.js
@@ -28,15 +28,31 @@
                 self[key] = value;
             });
 
+            self._dataPercent();
             self._build();
-
             var $container = self.$container = $(document.createElement("div")).insertBefore($el);
             $(document.createElement("div")).attr('class', 'radia-bar-container').appendTo($container)
                 .append(self.svgContainer);
 
             return $el.removeClass('radia-bar-loading');
         },
-
+        _dataPercent: function(){
+            var self = this;
+            var length = self.options["data"].length;
+            var data = self.options["data"];
+            if(length>0){
+                var percent = new Array(length);
+                var summary = 0;
+                for (let i = 0; i < length; i++) {
+                    summary = summary + parseInt(data[i].progress);
+                }
+                var relative = 100 / summary;
+                for (let i = 0; i < length; i++) {
+                    percent[i] = Math.round(relative*data[i].progress);
+                }
+                self.options["percent"] = percent;
+            }
+        },
         _build: function(){
             var self = this;
             
@@ -48,7 +64,7 @@
             }
 
             var r = self.radius = w / 3, paddingLabel = r + 20;
-            var scale = "-"+ (r * 1.5) +" -"+ (r) +" "+ w +" "+ h +"";
+            var scale = "-"+ (r * 1.5) +" -"+ (r * 1.5) +" "+ w +" "+ h +"";
             
             var strokeWidth = self.strokeWidth = self.options.strokeWidth || 12;
 
@@ -59,22 +75,28 @@
             var length = self.options["data"].length;
             var padding = self.options.padding || 20;
             var division = (r - padding) / length;
-
+            var maxValue = 0;
+            for (let i = 0; i < length; i++) {
+                if(parseInt(data[i].progress) > maxValue){
+                    maxValue = parseInt(data[i].progress);
+                }
+            }
+            var raiusCircle = w / 2;
             for(var i = 0; i < length; i++){
                 var object = self.options["data"][i];
+                var percent = self.options["percent"][i];
                 var stroreWidth = 12;
                 if(i<6){
                     stroreWidth = 12 - i;
-                    r = r + i;
                 }
                 else {
                     stroreWidth = 12 - 6;
-                    r = r + 6;
                 }
-                self._buildCircle(object, r, stroreWidth);
-                self._buildLabel(object, paddingLabel);
+                self._buildCircle(object, raiusCircle, stroreWidth, maxValue);
+                self._buildLabel(object, paddingLabel, percent);
                 paddingLabel += 20;
                 r = r - division;
+                raiusCircle = raiusCircle - division;
             }
         },
 
@@ -102,12 +124,13 @@
             $(feDropShadow).appendTo(shadow);
             $(shadow).appendTo(self.svgContainer);
         },
-        _buildCircle: function(object, r, stroreWidth){
+        _buildCircle: function(object, r, stroreWidth, maxValue){
             var self = this;
             var radius = r - 10;
             var circumference = radius * 2 * Math.PI;
 
-            var offset = circumference - object.progress / 100 * circumference;
+            // var offset = circumference - object.progress / 100 * circumference;
+            var offset = circumference - object.progress / maxValue * 0.9 * circumference;
 
             var circle = document.createElementNS(svgNS,"circle");
             circle.setAttribute("cy", 0);
@@ -129,32 +152,38 @@
             self.svgContainer.appendChild(clonedCircle);
         },
 
-        _buildLabel: function(object, padding){
+        _buildLabel: function(object, padding, percent){
             var self = this;
+            var legend = $(self.element).parents('.radialbarmultiple').find('.legend .legend-list');
+            var legendRow = 
+            '<div class="legend-row">'
+            +'    <div class="legend-value" style="background-color: ' + object.background + ';">'+ percent + '% / ' + object.progress + ' шт</div>'
+            +'    <div class="legend-label">Ответы на оценку <span class="bold">'+ object.labelText + '</span></div>'
+            +'</div>';
+            $(legendRow).appendTo(legend);
+            // var circle = document.createElementNS(svgNS,"circle");
+            // circle.setAttribute("cy", padding);
+            // circle.setAttribute("cx", -self.radius);
+            // circle.setAttribute("r", 5);
+            // circle.setAttribute("stroke-width", 8);
+            // circle.setAttribute("fill", "none");
+            // circle.setAttribute("stroke", '#F5F7FA');
 
-            var circle = document.createElementNS(svgNS,"circle");
-            circle.setAttribute("cy", padding);
-            circle.setAttribute("cx", -self.radius);
-            circle.setAttribute("r", 5);
-            circle.setAttribute("stroke-width", 8);
-            circle.setAttribute("fill", "none");
-            circle.setAttribute("stroke", '#F5F7FA');
+            // var clonedCircle = $(circle).clone()[0];
+            // clonedCircle.setAttribute("stroke", object.background);
+            // clonedCircle.setAttribute("r", 3);
+            // clonedCircle.setAttribute("fill", object.background);
 
-            var clonedCircle = $(circle).clone()[0];
-            clonedCircle.setAttribute("stroke", object.background);
-            clonedCircle.setAttribute("r", 3);
-            clonedCircle.setAttribute("fill", object.background);
+            // var text = document.createElementNS(svgNS,"text");
+            // text.setAttribute("y", padding + 5);
+            // text.setAttribute("x", -self.radius + 10);
+            // text.setAttribute("font-size", 12);
+            // var textNode = document.createTextNode(object.labelText);
+            // text.appendChild(textNode);
 
-            var text = document.createElementNS(svgNS,"text");
-            text.setAttribute("y", padding + 5);
-            text.setAttribute("x", -self.radius + 10);
-            text.setAttribute("font-size", 12);
-            var textNode = document.createTextNode(object.labelText);
-            text.appendChild(textNode);
-
-            self.svgContainer.appendChild(circle);
-            self.svgContainer.appendChild(clonedCircle);
-            self.svgContainer.appendChild(text);
+            // self.svgContainer.appendChild(circle);
+            // self.svgContainer.appendChild(clonedCircle);
+            // self.svgContainer.appendChild(text);
         },
 
         destroy: function() {
