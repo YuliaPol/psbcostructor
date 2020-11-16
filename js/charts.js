ba892790001a916a5c063051a5dbe0b7ef247dd7
YuliaPol <julia.polyeshko@gmail.com>
2020-11-16 09:44:59 +0200

    horizontal chart

--- js/charts.js
+++ js/charts.js
@@ -14,6 +14,9 @@ if(chartData.length > 0){
             if(chartData[i].type == 'simpleBar'){
                 drawSimpleBar($(chartData[i].element), data);
             }
+            if(chartData[i].type == 'horizontalbar'){
+                drawHorizontalBar(chartData[i].element, data);
+            }
         }
     }
 }
@@ -38,10 +41,11 @@ function drawSimpleBar(element, data) {
             percentFill[i] = Math.round(relativeFill*data[i].progress*0.9);
         }
         var elements = '<div class="barlist">';
+        var width = parseInt(data.length);
         for (let i = 0; i < data.length; i++) {
             var top = 100 - percentFill[i];
             elements = elements + 
-            '<div class="baritem">'
+            '<div class="baritem" style="max-width: calc(100%/' + width +');width: calc(100%/' + width +');">'
             +'    <div class="barcol">'
             +'      <div class="barcolFill" style="background-color: '+ data[i].background + '; top: ' + top + '%"></div>'
             +'      <div class="value">' + percent[i] + '% / '+ data[i].progress + ' шт</div>'
@@ -50,7 +54,103 @@ function drawSimpleBar(element, data) {
             +'</div>';
         }
         var elements = elements + '</div>';
-        element.addClass('.simplebar');
+        element.addClass('varticalbarChart');
         $(elements).appendTo(element);
+    }
+}
+function drawHorizontalBar(element, data){
+    if($(element).length==1){
+        var id = element.split('.')[1];
+        var canvas = '<canvas  id="' + id + '"></canvas>';
+        $(canvas).appendTo($(element));        
+        var newData = new Array(data.length);
+        var backgroundColor = new Array(data.length);
+        var labels = new Array(data.length);
+        for (let i = 0; i < data.length; i++) {
+            labels[i] = data[i].labelText;
+            backgroundColor[i] = data[i].background;
+            newData[i] = data[i].progress;
+        }
+
+        var ChartData = {
+            labels: labels,
+            datasets: [{
+                data: newData,
+                backgroundColor: backgroundColor,
+                hoverBackgroundColor: backgroundColor,
+                borderWidth: 0
+            }]
+        };
+
+        var horizontal = document.getElementById(id).getContext('2d');
+        var myBarChart = new Chart(horizontal, {
+            type: 'horizontalBar',
+            data: ChartData,
+            options: {
+                cornerRadius: 100,
+                cornerposition: 'right', //right, left, bottom, top, all
+                tooltips: {
+                    enabled: false
+                },
+                hover: {mode: null},
+                legend: {
+                    display: false,
+                },
+                layout: {
+                    padding: {
+                        left: 50//for text-align
+                    }
+                }, 
+                scales: {
+                    xAxes: [{
+                        gridLines: {
+                            color: '#D5D3D3',
+                            lineWidth: 1
+                        },
+                        ticks: {
+                            fontSize: 10,
+                            min: 0,
+                        }
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            color: '#D5D3D3',
+                            lineWidth: 1
+                        },
+                        ticks: {
+                            fontSize: 10,
+                            mirror: true,//for text-align
+                            padding: 50//for text-align
+                        }
+                    }],
+                },
+            },
+            plugins: [{
+                afterDraw: function(horizontal){    
+                    var ctx = horizontal.chart.ctx; 
+                    var yAxis = horizontal.scales['y-axis-0'];
+                    var bottom = yAxis.bottom;
+                    for (var index = 0; index < yAxis.ticks.length; index++) {
+                        var y = yAxis.getPixelForTick(index);  
+                        if (!window.document.documentMode) {
+                            if(index != 0){
+                                ctx.beginPath();
+                                ctx.moveTo(15,  y + 0.5);
+                                ctx.lineTo(yAxis.right,  y + 0.5);
+                                ctx.lineWidth = 1;
+                                ctx.strokeStyle = "#D5D3D3";
+                                ctx.stroke();
+                            }
+                        }
+                    }
+                    ctx.beginPath();
+                    ctx.moveTo(15,  bottom + 0.5);
+                    ctx.lineTo(yAxis.right,  bottom + 0.5);
+                    ctx.lineWidth = 1;
+                    ctx.strokeStyle = "#D5D3D3";
+                    ctx.stroke();
+                }
+            }],
+        });
     }
 }
